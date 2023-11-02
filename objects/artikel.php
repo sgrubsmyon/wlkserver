d637bb289b8cf241ab21c569b557f3279a8df11a
Markus Voge <sgrubsmyon@users.noreply.github.com>
2023-11-02 23:29:04 +0100

    Use lower case fo exists checks

--- objects/artikel.php
+++ objects/artikel.php
@@ -1,5 +1,6 @@
 <?php
-class Artikel {
+class Artikel
+{
   // database connection and table name
   private $conn;
 
@@ -14,7 +15,8 @@ class Artikel {
   //public $mwst_satz;
 
   // constructor with $db as database connection
-  public function __construct($db) {
+  public function __construct($db)
+  {
     $this->conn = $db;
   }
 
@@ -23,16 +25,17 @@ class Artikel {
    **************/
 
   // check if a product already exists in the DB
-  public function exists_by_lief_id($lieferant_id, $artikel_nr) {
+  public function exists_by_lief_id($lieferant_id, $artikel_nr)
+  {
     $query = "SELECT COUNT(*) > 0 AS ex FROM artikel
-    WHERE lieferant_id = ? AND artikel_nr = ? AND artikel.aktiv";
+    WHERE lieferant_id = ? AND LOWER(artikel_nr) = LOWER(?) AND artikel.aktiv";
 
     // prepare query statement
     $stmt = $this->conn->prepare($query);
     $stmt->bindParam(1, $lieferant_id);
     $stmt->bindParam(2, $artikel_nr);
 
-     // execute query
+    // execute query
     if ($stmt->execute()) {
       $row = $stmt->fetch(PDO::FETCH_ASSOC);
       return $row['ex'] == '1' ? true : false;
@@ -41,17 +44,18 @@ class Artikel {
     return null;
   }
 
-  function exists_by_lief_some_name($lieferant_name, $artikel_nr, $which_name) {
+  function exists_by_lief_some_name($lieferant_name, $artikel_nr, $which_name)
+  {
     $query = "SELECT COUNT(*) > 0 AS ex FROM artikel
     INNER JOIN lieferant USING (lieferant_id)
-    WHERE " . $which_name . " = ? AND artikel_nr = ? AND artikel.aktiv";
+    WHERE LOWER(" . $which_name . ") = LOWER(?) AND LOWER(artikel_nr) = LOWER(?) AND artikel.aktiv";
 
     // prepare query statement
     $stmt = $this->conn->prepare($query);
     $stmt->bindParam(1, $lieferant_name);
     $stmt->bindParam(2, $artikel_nr);
 
-     // execute query
+    // execute query
     if ($stmt->execute()) {
       $row = $stmt->fetch(PDO::FETCH_ASSOC);
       return $row['ex'] == '1' ? true : false;
@@ -60,12 +64,14 @@ class Artikel {
     return null;
   }
 
-  public function exists_by_lief_name($lieferant_name, $artikel_nr) {
+  public function exists_by_lief_name($lieferant_name, $artikel_nr)
+  {
     $res = $this->exists_by_lief_some_name($lieferant_name, $artikel_nr, "lieferant_name");
     return $res;
   }
 
-  public function exists_by_lief_kurzname($lieferant_kurzname, $artikel_nr) {
+  public function exists_by_lief_kurzname($lieferant_kurzname, $artikel_nr)
+  {
     $res = $this->exists_by_lief_some_name($lieferant_kurzname, $artikel_nr, "lieferant_kurzname");
     return $res;
   }
@@ -74,14 +80,41 @@ class Artikel {
    * CREATE     *
    **************/
 
-  public function create_by_lief_id($lieferant_id, $artikel_nr, $data) {
+  public function create_by_lief_id($lieferant_id, $artikel_nr, $data)
+  {
     if ($this->exists_by_lief_id($lieferant_id, $artikel_nr)) {
       // Throw error and do not create the article, as it exists already
-      return false;
+      return [
+        "success" => false,
+        "status" => 400,
+        // bad request
+        "error" => "Article with (lieferant_id, artikel_nr) = (" . $lieferant_id . ", " . $artikel_nr . ") already exists."
+      ];
     }
     $query = "INSERT INTO artikel SET 
     lieferant_id = ?, artikel_nr = ?";
-    return true;
+    foreach ($data as $field => $value) {
+      $query = $query . ", " . $field . " = " . $value;
+    }
+    print($query);
+
+    // // prepare query statement
+    // $stmt = $this->conn->prepare($query);
+    // $stmt->bindParam(1, $lieferant_id);
+    // $stmt->bindParam(2, $artikel_nr);
+
+    // // execute query
+    // if ($stmt->execute()) {
+    //   $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    //   return $row['ex'] == '1' ? true : false;
+    // }
+
+    return [
+      "success" => true,
+      "status" => 200,
+      // OK
+      "error" => ""
+    ];
   }
 
   /**************
@@ -92,14 +125,14 @@ class Artikel {
   artikel_id, produktgruppen_name AS produktgruppe, lieferant_name AS lieferant,
   artikel_nr, artikel_name, kurzname, barcode, menge, einheit, vpe, setgroesse,
   vk_preis, empf_vk_preis, ek_rabatt, ek_preis, variabler_preis,
-  herkunft,
-  sortiment, lieferbar, beliebtheit, bestand, von, bis, artikel.aktiv
+  herkunft, sortiment, lieferbar, beliebtheit, bestand, von, bis, artikel.aktiv
   FROM artikel
   INNER JOIN produktgruppe USING (produktgruppen_id)
   INNER JOIN lieferant USING (lieferant_id)";
 
-  public function read_active_by_lief_id($lieferant_id, $artikel_nr) {
-    $query = $this->read_query . " WHERE lieferant_id = ? AND artikel_nr = ? AND artikel.aktiv";
+  public function read_active_by_lief_id($lieferant_id, $artikel_nr)
+  {
+    $query = $this->read_query . " WHERE lieferant_id = ? AND LOWER(artikel_nr) = LOWER(?) AND artikel.aktiv";
     // return array("message" => $query);
 
     // prepare query statement
@@ -107,7 +140,7 @@ class Artikel {
     $stmt->bindParam(1, $lieferant_id);
     $stmt->bindParam(2, $artikel_nr);
 
-     // execute query
+    // execute query
     if ($stmt->execute()) {
       $row = $stmt->fetch(PDO::FETCH_ASSOC);
       return $row;
@@ -116,8 +149,9 @@ class Artikel {
     return null;
   }
 
-  function read_active_by_lief_some_name($lieferant_name, $artikel_nr, $which_name) {
-    $query = $this->read_query . " WHERE " . $which_name . " = ? AND artikel_nr = ? AND artikel.aktiv";
+  function read_active_by_lief_some_name($lieferant_name, $artikel_nr, $which_name)
+  {
+    $query = $this->read_query . " WHERE LOWER(" . $which_name . ") = LOWER(?) AND LOWER(artikel_nr) = LOWER(?) AND artikel.aktiv";
     // return array("message" => $query);
 
     // prepare query statement
@@ -125,7 +159,7 @@ class Artikel {
     $stmt->bindParam(1, $lieferant_name);
     $stmt->bindParam(2, $artikel_nr);
 
-     // execute query
+    // execute query
     if ($stmt->execute()) {
       $row = $stmt->fetch(PDO::FETCH_ASSOC);
       return $row;
@@ -134,19 +168,22 @@ class Artikel {
     return null;
   }
 
-  public function read_active_by_lief_name($lieferant_name, $artikel_nr) {
+  public function read_active_by_lief_name($lieferant_name, $artikel_nr)
+  {
     $res = $this->read_active_by_lief_some_name($lieferant_name, $artikel_nr, "lieferant_name");
     return $res;
   }
 
-  public function read_active_by_lief_kurzname($lieferant_kurzname, $artikel_nr) {
+  public function read_active_by_lief_kurzname($lieferant_kurzname, $artikel_nr)
+  {
     $res = $this->read_active_by_lief_some_name($lieferant_kurzname, $artikel_nr, "lieferant_kurzname");
     return $res;
   }
 
   // return all articles, not only the currently active one (this is the 'article history')
-  public function read_all_by_lief_id($lieferant_id, $artikel_nr) {
-    $query = $this->read_query . " WHERE lieferant_id = ? AND artikel_nr = ? ORDER BY artikel_id DESC";
+  public function read_all_by_lief_id($lieferant_id, $artikel_nr)
+  {
+    $query = $this->read_query . " WHERE lieferant_id = ? AND LOWER(artikel_nr) = LOWER(?) ORDER BY artikel_id DESC";
     // return array("message" => $query);
 
     // prepare query statement
@@ -154,7 +191,7 @@ class Artikel {
     $stmt->bindParam(1, $lieferant_id);
     $stmt->bindParam(2, $artikel_nr);
 
-     // execute query
+    // execute query
     if ($stmt->execute()) {
       // artikel array
       $artikel_arr = array();
@@ -174,8 +211,9 @@ class Artikel {
     return null;
   }
 
-  function read_all_by_lief_some_name($lieferant_name, $artikel_nr, $which_name) {
-    $query = $this->read_query . " WHERE " . $which_name . " = ? AND artikel_nr = ? ORDER BY artikel_id DESC";
+  function read_all_by_lief_some_name($lieferant_name, $artikel_nr, $which_name)
+  {
+    $query = $this->read_query . " WHERE LOWER(" . $which_name . ") = LOWER(?) AND LOWER(artikel_nr) = LOWER(?) ORDER BY artikel_id DESC";
     // return array("message" => $query);
 
     // prepare query statement
@@ -183,7 +221,7 @@ class Artikel {
     $stmt->bindParam(1, $lieferant_name);
     $stmt->bindParam(2, $artikel_nr);
 
-     // execute query
+    // execute query
     if ($stmt->execute()) {
       // artikel array
       $artikel_arr = array();
@@ -203,12 +241,14 @@ class Artikel {
     return null;
   }
 
-  public function read_all_by_lief_name($lieferant_name, $artikel_nr) {
+  public function read_all_by_lief_name($lieferant_name, $artikel_nr)
+  {
     $res = $this->read_all_by_lief_some_name($lieferant_name, $artikel_nr, "lieferant_name");
     return $res;
   }
 
-  public function read_all_by_lief_kurzname($lieferant_kurzname, $artikel_nr) {
+  public function read_all_by_lief_kurzname($lieferant_kurzname, $artikel_nr)
+  {
     $res = $this->read_all_by_lief_some_name($lieferant_kurzname, $artikel_nr, "lieferant_kurzname");
     return $res;
   }
