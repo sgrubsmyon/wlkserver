14bb0560313aad413728636ac3fbc088958e7e94
Markus Voge <sgrubsmyon@users.noreply.github.com>
2023-11-03 13:40:18 +0100

    Successfully read POST body data

--- artikel/create.php
+++ artikel/create.php
@@ -20,8 +20,9 @@ $db = $database->getConnection();
 $artikel = new Artikel($db);
 
 // read parameters from POST method
-$lieferant_id = $_POST['lieferant_id'];
-$artikel_nr = isset($_POST['artikel_nr']) ? $_POST['artikel_nr'] : null;
+$POST_DATA = json_decode(file_get_contents('php://input'), true); // https://stackoverflow.com/questions/18866571/receive-json-post-with-php
+$lieferant_id = isset($POST_DATA['lieferant_id']) ? $POST_DATA['lieferant_id'] : null;
+$artikel_nr = isset($POST_DATA['artikel_nr']) ? $POST_DATA['artikel_nr'] : null;
 
 if (is_null($lieferant_id)) {
   // set response code - 400 bad request
@@ -53,8 +54,7 @@ if (is_null($artikel_nr)) {
   die();
 }
 
-$artikel_data = isset($_POST['data']) ? $_POST['data'] : null;
-print($artikel_data);
+$artikel_data = isset($POST_DATA['data']) ? $POST_DATA['data'] : null;
 $ret = $artikel->create_by_lief_id($lieferant_id, $artikel_nr, $artikel_data);
 
 http_response_code($ret["status"]);
