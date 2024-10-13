9d5033c1dd8b7223c43f651a02cc3e7a8845eed8
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-10-14 00:25:12 +0200

    First test of using multiple files with FastAPI

--- python/main.py
+++ python/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
-app = FastAPI()
+from .dependencies import get_query_token, get_token_header
+# from .internal import admin
+from .routers import artikel, lieferanten
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+
+app.include_router(artikel.router)
+app.include_router(lieferanten.router)
+# app.include_router(
+#     admin.router,
+#     prefix="/admin",
+#     tags=["admin"],
+#     dependencies=[Depends(get_token_header)],
+#     responses={418: {"description": "I'm a teapot"}},
+# )
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Bigger Applications!"}
---
+++ python/routers/artikel.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from ..dependencies import get_token_header
+
+router = APIRouter(
+    prefix="/artikel",
+    tags=["artikel"],
+    dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
+
+fake_artikel_db = {"cr01": {"bezeichnung": "Costa Rica Cola"}, "ros": {"bezeichnung": "Rosinen"}}
+
+
+@router.get("/")
+async def read_artikel():
+    return fake_artikel_db
+
+
+@router.get("/{artikel_id}")
+async def read_single_artikel(artikel_id: str):
+    if artikel_id not in fake_artikel_db:
+        raise HTTPException(status_code=404, detail="Artikel not found")
+    return {"bezeichnung": fake_artikel_db[artikel_id]["bezeichnung"], "artikel_id": artikel_id}
+
+
+@router.put(
+    "/{artikel_id}",
+    tags=["artikel"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_artikel(artikel_id: str):
+    if artikel_id != "cr01":
+        raise HTTPException(
+            status_code=403, detail="You can only update the artikel: cr01"
+        )
+    return {"artikel_id": artikel_id, "name": "The great Costa Rica Cola"}
