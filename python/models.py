7625578a35f1534e6ce09414194e694aa3b06ce1
Markus Voge <sgrubsmyon@users.noreply.github.com>
2024-12-04 01:20:03 +0100

    Add product group models

--- python/models.py
+++ python/models.py
@@ -37,24 +37,62 @@ class Pfand(SQLModel, table=True):
     produktgruppe: list["Produktgruppe"] = Relationship(back_populates="pfand")
 
 
-class Produktgruppe(SQLModel, table=True):
-    __tablename__ = 'produktgruppe'
+#######################
+# Produktgruppe models
+#######################
 
-    produktgruppen_id: int = Field(primary_key=True)
+# The base model, shared by all
+class ProduktgruppeBase(SQLModel):
     toplevel_id: int = Field(default=1) # sa_column_kwargs={"unsigned": True}, 
     sub_id: Optional[int] = Field() # sa_column_kwargs={"unsigned": True}
     subsub_id: Optional[int] = Field() # sa_column_kwargs={"unsigned": True}
     produktgruppen_name: str = Field(max_length=50, nullable=False)
-    mwst_id: Optional[int] = Field(foreign_key="mwst.mwst_id") # sa_column_kwargs={"unsigned": True}, 
-    pfand_id: Optional[int] = Field(foreign_key="pfand.pfand_id") # sa_column_kwargs={"unsigned": True}, 
     std_einheit: Optional[str] = Field(max_length=10)
     n_artikel: Optional[int] = Field() # sa_column_kwargs={"unsigned": True}
     n_artikel_rekursiv: Optional[int] = Field() # sa_column_kwargs={"unsigned": True}
-    aktiv: bool = Field(nullable=False, default=True)
 
+# The table model
+class Produktgruppe(ProduktgruppeBase, table=True):
+    __tablename__ = 'produktgruppe'
+
+    produktgruppen_id: int = Field(primary_key=True)
+    mwst_id: Optional[int] = Field(foreign_key="mwst.mwst_id") # sa_column_kwargs={"unsigned": True}, 
+    pfand_id: Optional[int] = Field(foreign_key="pfand.pfand_id") # sa_column_kwargs={"unsigned": True}, 
+
+    aktiv: bool = Field(nullable=False, default=True)
+    
+    # relationships
     artikel: list["Artikel"] = Relationship(back_populates="produktgruppe")
     mwst: Mwst = Relationship(back_populates="produktgruppe")
     pfand: Pfand = Relationship(back_populates="produktgruppe")
+
+# For reading product groups
+class ProduktgruppePublic(ProduktgruppeBase):
+    produktgruppen_id: int
+    mwst_id: int
+    pfand_id: int
+
+    aktiv: bool
+
+
+# For creating product groups
+class ProduktgruppeCreate(ProduktgruppeBase):
+    mwst_id: int | None = None
+    pfand_id: int | None = None
+
+
+# For updating product groups
+class ProduktgruppeUpdate(SQLModel):
+    mwst_id: int | None = None
+    pfand_id: int | None = None
+
+    toplevel_id: int | None = None
+    sub_id: int | None = None
+    subsub_id: int | None = None
+    produktgruppen_name: str | None = None
+    std_einheit: str | None = None
+    n_artikel: int | None = None
+    n_artikel_rekursiv: int | None = None
 
 
 #################
@@ -90,25 +128,6 @@ class Artikel(ArtikelBase, table=True):
     # id = Column(Integer, Sequence('user_id_seq', start=100, increment=1), primary_key=True)
     produktgruppen_id: int = Field(foreign_key="produktgruppe.produktgruppen_id", nullable=False, default=8)
     lieferant_id: int = Field(foreign_key="lieferant.lieferant_id", nullable=False, default=1)
-    
-    # artikel_nr: str = Field(max_length=30, nullable=False)
-    # artikel_name: str = Field(max_length=180, nullable=False)
-    # kurzname: Optional[str] = Field(max_length=50)
-    # menge: Optional[float] = Field(sa_column=DECIMAL(precision=8, scale=5))
-    # einheit: Optional[str] = Field(max_length=10)
-    # barcode: Optional[str] = Field(max_length=30)
-    # herkunft: Optional[str] = Field(max_length=100)
-    # vpe: Optional[int] = Field() # sa_column_kwargs={"unsigned": True}
-    # setgroesse: int = Field(nullable=False, default=1) # sa_column_kwargs={"unsigned": True}, 
-    # vk_preis: Optional[float] = Field(sa_column=DECIMAL(precision=13, scale=2))
-    # empf_vk_preis: Optional[float] = Field(sa_column=DECIMAL(precision=13, scale=2))
-    # ek_rabatt: Optional[float] = Field(sa_column=DECIMAL(precision=6, scale=5))
-    # ek_preis: Optional[float] = Field(sa_column=DECIMAL(precision=13, scale=2))
-    # variabler_preis: bool = Field(nullable=False, default=False)
-    # sortiment: bool = Field(nullable=False, default=False)
-    # lieferbar: bool = Field(nullable=False, default=False)
-    # beliebtheit: int = Field(nullable=False, default=0)
-    # bestand: Optional[int] = Field() # sa_column_kwargs={"unsigned": True}
 
     von: Optional[datetime] = Field(nullable=True, default=None)
     bis: Optional[datetime] = Field(nullable=True, default=None)
